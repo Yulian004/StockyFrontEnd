e6c8aa587bd10288b8d8ba8c5a54d67895a63399
pelli <grazknof@gmail.com>
2025-10-22 16:50:26 +0200

    Aggiutna login e collegamento

---
+++ src/login/LoginTs.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.html',
+  imports: [
+    FormsModule,
+  ],
+  styleUrls: ['./loginCSS.css']
+})
+export class LoginComponent {
+  user: string = '';
+  password: string = '';
+  loading: boolean = false; // La roba del bottone, da ignorare per backend
+  errorMessage: string = ''; // Serve il popup errore
+
+  constructor(private http: HttpClient) {} // Chiamata all'Injection
+
+  onLogin() {
+    this.errorMessage = '';
+    this.loading = true; // Sta roba resfresha il messaggio d'errore così non compare ogni volta
+
+    if (!this.user || !this.password) {
+      this.errorMessage = 'Compila tutti i campi.';
+      this.loading = false;
+      return;
+    }
+
+    const loginData = {
+      user: this.user,
+      password: this.password
+    };
+
+    // Roba Backend
+    this.http.post('api/login', loginData)
+      .subscribe({
+        next: (response: any) => {
+          alert('Login effettuato con successo ✅');
+          this.loading = false;
+        },
+        error: (err) => {
+          this.errorMessage = 'Credenziali non valide.';
+          this.loading = false;
+        }
+      });
+  }
+}
