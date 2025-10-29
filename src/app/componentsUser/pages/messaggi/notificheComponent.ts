f067127c0b353317230a47cc792b26908a2ef018
Alice <alicequintili@hotmail.com>
2025-10-29 18:14:51 +0100

    Modifiche grafiche

---
+++ src/app/componentsUser/pages/messaggi/notificheComponent.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-notifications-page',
+  templateUrl: './notificheComponent.html',
+  styleUrls: ['./notificheComponent.css']
+})
+
+
+
+export class NotificheComponent {
+}
--- src/app/componentsUser/parts/navbar/navbar.ts
+++ src/app/componentsUser/parts/navbar/navbar.ts
@@ -1,9 +1,11 @@
 import  { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   imports: [
+    CommonModule,
     RouterLink,
     RouterLinkActive
   ],
@@ -11,8 +13,25 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  user = {
+    avatarUrl: '' // URL reale dell'immagine dell'utente
+  };
 
-}
+  defaultAvatar = 'https://static.vecteezy.com/ti/vetor-gratis/p3/23016923-gato-simbolo-ilustracao-gratis-vetor.jpg';
 
-export class NavbarComponent {
+  dropdownOpen = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  logout() {
+    console.log('Utente disconnesso');
+    // qui inserisci la logica reale di logout
+  }
+
+  onImageError(event: any) {
+    event.target.src = this.defaultAvatar;
+  }
+
 }
